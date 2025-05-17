1ffc562d7430ce3eba926d72e5fe0ad57fc0e565
wiqaaas <wiqaaas@gmail.com>
2025-05-16 23:14:50 -0230

    added test colab

---
+++ test_colab.py
@@ -0,0 +1,8 @@
+# run in colab but will not work as there is not TWS Desktop App there.
+
+# !rm -rf trading
+# !git clone https://github.com/wiqaaas/trading.git
+# %cd trading
+# !pip install .
+# from Test import ib_contract_async
+# ib_contract_async.main()
